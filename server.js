cf9903d520ccac58631f537b2f399a9ee493b59b
Abdullah111000 <abdullahjutt1100@gmail.com>
2026-08-13 12:34:00 +0500

    Add vercel.json and update CORS

--- server.js
+++ server.js
@@ -29,7 +29,13 @@ configureCloudinary();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        return callback(null, origin);
+    },
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -51,3 +57,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
